b7fc85808258203e01ccf3b2014d6feb5ab9e017
Juan Felipe <juan@jfocampo.com>
2019-04-17 10:51:02 -0400

    Project upload

---
+++ precache-manifest.a72885be5b0cee5ac1b7bfdf50dc925f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1668aa55467f4a25c6e",
+    "url": "./static/css/main.36175357.chunk.css"
+  },
+  {
+    "revision": "d1668aa55467f4a25c6e",
+    "url": "./static/js/main.895bcf37.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "e1df9e5dd97f9dc5b41a",
+    "url": "./static/js/2.373ccd69.chunk.js"
+  },
+  {
+    "revision": "25ea11551baba0b899ee3377834a9c9d",
+    "url": "./static/media/CalibreWeb-Regular.25ea1155.ttf"
+  },
+  {
+    "revision": "d36e8e99fa694666f6bf842ec302955d",
+    "url": "./static/media/CalibreWeb-Semibold.d36e8e99.ttf"
+  },
+  {
+    "revision": "4e536581b6ba0defebc70e4cc47db610",
+    "url": "./static/media/dm-icons.4e536581.eot"
+  },
+  {
+    "revision": "ca5c167616173c7d757371245d2fb7ec",
+    "url": "./static/media/dm-icons.ca5c1676.woff"
+  },
+  {
+    "revision": "f768f4eee5821da0227bc67fb925b133",
+    "url": "./static/media/dm-icons.f768f4ee.ttf"
+  },
+  {
+    "revision": "c79bad44804f5fee62012b3cddff1acb",
+    "url": "./static/media/dm-icons.c79bad44.svg"
+  },
+  {
+    "revision": "b4734ada836f0d3ef584de2d10bdefd0",
+    "url": "./index.html"
+  }
+];
